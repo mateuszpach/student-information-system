b9d90cb4b473734213b25da5ba7aee2bf18ec1d0
Lukasz Gniecki <lukasgniecki@gmail.com>
2020-06-05 19:17:21 +0200

    Statistics about frequency

--- database/zajecia.sql
+++ database/zajecia.sql
@@ -231,3 +231,72 @@ begin
     );
 end
 $$ language 'plpgsql';
+
+
+-- obecnosci statystyki
+
+create or replace function poczatek_semestru() returns date as $$
+declare
+    czas date = now()::date;
+begin
+    while to_char(czas, 'Mon') != 'Sep' and to_char(czas, 'Mon') != 'Feb' loop
+        czas := czas - '1 Month'::interval;
+    end loop;
+    return date_trunc('Month', czas)::date;
+end
+$$ language 'plpgsql';
+
+create or replace function liczba_status(id_ucznia int, status statusobecnosci) returns int as $$
+declare
+begin
+    return (
+        select count(*)
+        from obecnosci o
+        join instancje_zajec iz on o.instancja_zajecia = iz.id_instancji
+        where o.uczen = id_ucznia
+        and o.status = liczba_status.status
+        and iz.data between poczatek_semestru() and now()::date
+    );
+end
+$$ language 'plpgsql';
+
+
+create or replace function liczba_zajec(id_ucznia int) returns int as $$
+declare
+begin
+    return (
+        select count(*)
+        from instancje_zajec iz
+        join uczniowie_view uv on iz.klasa = uv.klasa
+        where uv.id_osoby = id_ucznia
+        and iz.data between poczatek_semestru() and now()::date
+    );
+end
+$$ language 'plpgsql';
+
+
+create or replace function zestawienie_obecnosci(id_wych int)
+returns table (
+    uczen text,
+    obecnosc_proc numeric,
+    obecnosci int,
+    nieobecnosci int,
+    ucieczki int,
+    nieobecnosci_u int,
+    zwolnienia int
+) as $$
+declare
+begin
+    return query (
+        select concat(uv.imie, ' ', uv.nazwisko),
+               liczba_status(uv.id_osoby, 'O')::numeric / greatest(liczba_zajec(uv.id_osoby), 1)::numeric * 100,
+               liczba_status(uv.id_osoby, 'O'),
+               liczba_status(uv.id_osoby, 'N'),
+               liczba_status(uv.id_osoby, 'U'),
+               liczba_status(uv.id_osoby, 'NU'),
+               liczba_status(uv.id_osoby, 'Z')
+        from uczniowie_view uv
+        where uv.klasa = klasa_wychowawcy(id_wych)
+    );
+end
+$$ language 'plpgsql';
