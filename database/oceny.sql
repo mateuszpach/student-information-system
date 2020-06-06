17edece09b8368881ccee8a7b00fdc4792c739ad
mateuszpach <mateusz.pach.05@gmail.com>
2020-06-06 15:31:49 +0200

    merge

--- database/oceny.sql
+++ database/oceny.sql
@@ -41,8 +41,6 @@ begin
 end;
 $$ language 'plpgsql';
 
-
-
 --TODO zrób jako tabelkę, w której widnieją wszystkie przedmioty i całościowa średnia
 create or replace view wyniki_w_nauce as
 select k.nazwa as klasa, srednia_klasy(k.id_klasy) as srednia
